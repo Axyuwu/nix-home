5200f8d7c2f68bbd25174107a8ebc6be20f4929e
Axy <gilliardmarthey.axel@gmail.com>
2025-01-08 05:22:12 +0100

    Small progress on neovim configuration

--- nvim/default.nix
+++ nvim/default.nix
@@ -16,7 +16,27 @@ in {
       description = "What neovim package to use";
     };
   };
-  config = lib.mkIf cfg.enable {
-    programs.neovim = { enable = true; package = cfg.package; };
-  };
+  config = lib.mkIf cfg.enable ({
+    home.packages = [ cfg.package ];
+    home.file.".config/nvim/init.lua".source = pkgs.writeText "neovim_init_file" (''
+      vim.g.mapleader = ' '
+      vim.g.maplocalleader = ' '
+      vim.g.have_nerd_font = true
+      vim.opt.number = true
+      vim.opt.mouse = 'a'
+      vim.opt.clipboard = 'unnamedplus'
+      vim.opt.breakindent = true
+      vim.opt.undofile = true
+      vim.opt.ignorecase = true
+      vim.opt.smartcase = true
+      vim.opt.signcolumn = 'yes'
+      vim.opt.updatetime = 250
+      vim.opt.timeoutlen = 300
+      vim.opt.splitright = true
+      vim.opt.splitbelow = true
+      vim.opt.inccommand = 'split'
+      vim.opt.cursorline = true
+      vim.opt.scrolloff = 20
+    '');
+  });
 }
