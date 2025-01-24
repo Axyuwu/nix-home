c96037afdc943fac7fabef784dd33b742756212b
Axy <gilliardmarthey.axel@gmail.com>
2025-01-24 16:39:13 +0100

    Added lualine.nvim, made command line 0 chars long by default

--- nvim/default.nix
+++ nvim/default.nix
@@ -19,62 +19,14 @@ in {
   config.programs.neovim = lib.mkIf cfg.enable {
     enable = true;
     package = cfg.package;
-    extraLuaConfig = ''
-      vim.keymap.set('n', ' ', '<Nop>')
-      vim.g.mapleader = ' '
-      vim.g.maplocalleader = ' '
-      vim.g.have_nerd_font = true
-      vim.opt.number = true
-      vim.opt.mouse = 'a'
-      vim.opt.clipboard = 'unnamedplus'
-      vim.opt.breakindent = true
-      vim.opt.undofile = true
-      vim.opt.ignorecase = true
-      vim.opt.smartcase = true
-      vim.opt.signcolumn = 'yes'
-      vim.opt.updatetime = 250
-      vim.opt.timeoutlen = 300
-      vim.opt.splitright = true
-      vim.opt.splitbelow = true
-      vim.opt.inccommand = 'split'
-      vim.opt.cursorline = true
-      vim.opt.scrolloff = 10
-
-      require 'nvim-treesitter.configs'.setup {}
-
-      local fzf = require 'fzf-lua'
-      fzf.setup {}
-      fzf.register_ui_select()
-
-      local wk = require 'which-key'
-      wk.setup {}
-      wk.add({ 
-        {"<leader>?", function() wk.show() end, desc = "Buffer Local Keymaps (which-key)"},
-	{
-	  {"<leader>w", "<cmd>w<cr>", desc = "Write"},
-	},
-	{
-	  {"gd", function() vim.lsp.buf.definition() end, desc = "Jump to definition"},
-	  {"<leader>d", function() vim.diagnostic.open_float(nil, {focus=true, scope = "cursor"}) end, desc = "Hover diagnostics"},
-	},
-	{
-	  {"<leader>l", group = "LSP"},
-          {"<leader>la", function() vim.lsp.buf.code_action() end, desc = "Code Action"},
-          {"<leader>lq", function() vim.lsp.buf.code_action {only = {"quickfix"}} end, desc = "Quick Fix"},
-          {"<leader>lr", function() vim.lsp.buf.rename() end, desc = "Rename Symbol"},
-	},
-      })
-
-      local lspconfig = require 'lspconfig'
-      lspconfig.nixd.setup {}
-      lspconfig.rust_analyzer.setup {}
-    '';
+    extraLuaConfig = builtins.readFile ./init.lua;
     plugins = with pkgs.vimPlugins; [
       nvim-lspconfig
       nvim-treesitter.withAllGrammars
       which-key-nvim
       fzf-lua
+      lualine-nvim
     ];
   };
-  config.home.packages = with pkgs; [ nixd rust-analyzer fzf ];
+  config.home.packages = with pkgs; [ nixd rust-analyzer fzf lua-language-server ];
 }
---
+++ nvim/init.lua
@@ -0,0 +1,80 @@
+vim.keymap.set('n', ' ', '<Nop>')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.cmdheight = 0;
+
+--vim.cmd("set cmdheight=0")
+
+require 'nvim-treesitter.configs'.setup {}
+
+local fzf = require 'fzf-lua'
+fzf.setup {}
+fzf.register_ui_select()
+
+local lualine = require 'lualine';
+lualine.setup {}
+
+local wk = require 'which-key'
+wk.setup {}
+wk.add({
+    {"<leader>?", function() wk.show() end, desc = "Buffer Local Keymaps (which-key)"},
+    {
+        {"<leader>w", "<cmd>w<cr>", desc = "Write"},
+    },
+    {
+        {"gd", function() vim.lsp.buf.definition() end, desc = "Jump to definition"},
+	{"<leader>d", function() vim.diagnostic.open_float(nil, {focus=true, scope = "cursor"}) end, desc = "Hover diagnostics"},
+    },
+    {
+        {"<leader>l", group = "LSP"},
+        {"<leader>la", function() vim.lsp.buf.code_action() end, desc = "Code Action"},
+        {"<leader>lq", function() vim.lsp.buf.code_action {only = {"quickfix"}} end, desc = "Quick Fix"},
+        {"<leader>lr", function() vim.lsp.buf.rename() end, desc = "Rename Symbol"},
+    },
+})
+
+local lspconfig = require 'lspconfig'
+lspconfig.nixd.setup {}
+lspconfig.rust_analyzer.setup {}
+lspconfig.lua_ls.setup {
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+                return
+            end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                version = 'LuaJIT'
+            },
+
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME
+                }
+            }
+        })
+    end,
+    settings = {
+        Lua = {}
+    }
+}
